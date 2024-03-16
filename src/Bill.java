6c6cf4308fb41d524012f338c90b5f3704cbaf04
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-15 21:30:18 -0300

    translate

--- src/Bill.java
+++ src/Bill.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 
 public class Bill {
     private static ArrayList<Bill> bills = new ArrayList<>();
-    private String produto;
-    private double valor;
+    private String product;
+    private double value;
     private int qtd;
-    private double imposto;
-    private double valorFinal;
+    private double tax;
+    private double finalValue;
 
-    public Bill(String produto, double valor, int qtd, double imposto, double valorFinal) {
-        this.produto = produto;
-        this.valor = valor;
+    public Bill(String product, double value, int qtd, double tax, double finalValue) {
+        this.product = product;
+        this.value = value;
         this.qtd = qtd;
-        this.imposto = imposto;
-        this.valorFinal = valorFinal;
+        this.tax = tax;
+        this.finalValue = finalValue;
         bills.add(this);
     }
 
@@ -21,11 +21,11 @@ public class Bill {
         StringBuilder result = new StringBuilder();
         for (Bill bill : bills) {
             result.append("=======================================================\n")
-                    .append("Produto: ").append(bill.produto).append("\n")
-                    .append("Valor sem imposto: ").append(bill.valor).append("\n")
+                    .append("Produto: ").append(bill.product).append("\n")
+                    .append("Valor sem imposto: ").append(bill.value).append("\n")
                     .append("Quantidade do produto: ").append(bill.qtd).append("\n")
-                    .append("Valor do imposto: ").append(bill.imposto).append("\n")
-                    .append("Valor final: ").append(bill.valorFinal).append("\n")
+                    .append("Valor do imposto: ").append(bill.tax).append("\n")
+                    .append("Valor final: ").append(bill.finalValue).append("\n")
                     .append("=======================================================\n");
         }
         return result.toString();
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import java.awt.*;
-import java.util.Arrays;
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá seja bem vindo a loja");
